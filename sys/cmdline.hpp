0627d132a957e1123a33806864c00ae7ff4072ed
yakubouski <yakubouski@users.noreply.github.com>
2018-04-11 16:14:15 +0300

    Update cmdline.hpp

--- sys/cmdline.hpp
+++ sys/cmdline.hpp
@@ -19,19 +19,7 @@ namespace atl {
 				const char*		long_name;
 				int				required;
 				const char*		value;
-
-				inline string get_short() const {
-					string name("");
-					if (short_name) {
-						name += short_name;
-						if (required) {
-							name += ':';
-						}
-					}
-					return name;
-				}
 				operator struct ::option() const { return { long_name,required ,nullptr,short_name }; }
-
 			public:
 				option(const char* long_name, int required = 0, const char short_name = 0) : short_name(short_name), long_name(long_name), required(required), value(nullptr) { ; }
 				option(const char* long_name, const char short_name, int required, const char* value) : short_name(short_name), long_name(long_name), required(required), value(value) { ; }
@@ -53,26 +41,30 @@ namespace atl {
 			typedef std::unordered_set<option, option::hash, option::equal>	options_set;
 
 		protected:
-			options_set options_list;
-
-
+			options_set		options_list;
+			string			program_name;
 		public:
 			/*
 			* Define options and parse command line
 			*/
 			template<typename ... ARGS>
-			void options(int argc, char* argv[], ARGS&& ... args) const throw (int) {
+			void options(int argc, char* argv[], ARGS&& ... args) throw (int) {
 				vector<struct ::option> options_long = { args...,{0,0,nullptr,0} };
 				array<int, 256>	options_map;
 				string options_short("");
 				int index = 0;
 				options_map.fill(0);
+				program_name = argv[0];
 
 				for (auto&& it : options_long) {
 					if (it.val) {
 						options_short += (char)it.val;
-						if (it.has_arg) {
+						switch (it.has_arg) {
+						case 2:
 							options_short += ':';
+						case 1:
+							options_short += ':';
+							break;
 						}
 					}
 					options_map[it.val] = index++;
@@ -82,19 +74,38 @@ namespace atl {
 				while ((opt = getopt_long(argc, argv, options_short.c_str(), options_long.data(), &option_index)) != -1) {
 					if (opt == 0) {
 						auto && o = options_long[option_index];
-						((command_line*)this)->options_list.emplace(o.name, (char)o.val, o.has_arg, optarg);
+						options_list.emplace(o.name, (char)o.val, o.has_arg, optarg);
 						if (o.has_arg == 1 && optarg == nullptr) {
 							throw option_index;
 						}
 					}
 					else if (opt != '?') {
 						auto && o = options_long[options_map[opt]];
-						((command_line*)this)->options_list.emplace(o.name, (char)o.val, o.has_arg, optarg);
+						options_list.emplace(o.name, (char)o.val, o.has_arg, optarg);
 						if (o.has_arg == 1 && optarg == nullptr) {
 							throw options_map[opt];
 						}
 					}
 				}
+			}
+			/*
+			* Get program name
+			*/
+			const char* program() const {
+				auto it = program_name.find_last_of('/');
+				return program_name.c_str() + (it != string::npos ? it + 1 : 0);
+			}
+			/*
+			* Get program working directory
+			*/
+			const char* pwd() const {
+				return std::getenv("PWD");
+			}
+			/*
+			* Get program executable string
+ 			*/
+			const char* exec_name() const {
+				return program_name.c_str();
 			}
 			/*
 			* Check options exist
@@ -137,6 +148,5 @@ namespace atl {
 				return default_value;
 			}
 		};
-		static const command_line cmdline;
 	}
 }
